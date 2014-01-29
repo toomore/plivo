ac390bed0b93880d72717e18330dca08d2a089c8
Toomore Chiang <toomore0929@gmail.com>
2014-01-30 04:08:06 +0800

    Fixed post json data.

--- plivoSMS.py
+++ plivoSMS.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
+import json
 import pprint
 import requests
 import setting
 
 
 def send_sms(data):
+    headers = {'content-type': 'application/json'}
     endpoint = 'https://api.plivo.com/v1/Account/%s/Message/' % setting.auth_id
-    result = requests.post(endpoint, data=data,
+    result = requests.post(endpoint, data=json.dumps(data), headers=headers,
                            auth=(setting.auth_id, setting.auth_token))
-    return result
+    return result.json()
 
 if __name__ == '__main__':
     text = u'這是一封測試簡訊 This a test SMS.'
     data = {
-            'src': '',
-            'dst': '',
+            'src': setting.msg_from,
+            'dst': setting.msg_to,
             'text': text*2 + str(len(text*2)),
            }
     pprint(send_sms(data))
