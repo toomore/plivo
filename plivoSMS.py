631928eb50f9c04e38e107d5b28c14770344ddb9
Toomore Chiang <toomore0929@gmail.com>
2014-01-30 13:39:01 +0800

    Classful.

--- plivoSMS.py
+++ plivoSMS.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
 import json
+import os
 import requests
 import setting
 from pprint import pprint
 
 
-def send_sms(data):
-    headers = {'content-type': 'application/json'}
-    endpoint = 'https://api.plivo.com/v1/Account/%s/Message/' % setting.auth_id
-    result = requests.post(endpoint, data=json.dumps(data), headers=headers,
-                           auth=(setting.auth_id, setting.auth_token))
-    return result.json()
+class plivo(object):
+    def __init__(self, auth_id, auth_token, api_version='v1',
+            api_url='https://api.plivo.com/'):
+        self.auth_id = auth_id
+        self.auth_token = auth_token
+        self.api_url = os.path.join(api_url, api_version)
+
+    def send_sms(data):
+        headers = {'content-type': 'application/json'}
+        endpoint = os.path.join(self.api_url,
+                                'Account/%s/Message/' % setting.auth_id)
+        result = requests.post(endpoint, data=json.dumps(data), headers=headers,
+                               auth=(self.auth_id, self.auth_token))
+        return result.json()
 
 if __name__ == '__main__':
     text = u'這是一封測試簡訊 This a test SMS.'*2
