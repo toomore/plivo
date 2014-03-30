c58850b0182a56bf343678839a67067acea64bde
Toomore Chiang <toomore0929@gmail.com>
2014-03-30 22:51:06 +0800

    Update docs.

--- plivo_api.py
+++ plivo_api.py
@@ -16,7 +16,7 @@ class Plivo(object):
         :param str api_version: plivo api version, default 'v1'
         :param str api_url: plivo api url
         :param str to_number: default to number
-        :param str source: default from number
+        :param iter source: default from number
     '''
     def __init__(self, auth_id, auth_token, api_version='v1',
             api_url='https://api.plivo.com/', to_number=None, source=None):
