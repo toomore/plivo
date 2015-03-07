6d3c5056503fcba2c7a07dc92bf921720792a522
Toomore Chiang <toomore0929@gmail.com>
2015-03-07 19:39:50 +0800

    Tiny changed.

--- plivo-go/main.go
+++ plivo-go/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"strings"
 )
 
 type Plivo struct {
@@ -17,35 +17,40 @@ type Plivo struct {
 
 func (p *Plivo) send(data map[string]string) {
 	json_data, _ := json.Marshal(data)
-	a, _ := http.NewRequest("POST", fmt.Sprintf("%s/%s/Message/", p.Host, p.User), strings.NewReader(string(json_data)))
+	a, _ := http.NewRequest("POST", fmt.Sprintf("%s/%s/Message/", p.Host, p.User), bytes.NewReader(json_data))
 	a.URL.User = url.UserPassword(p.User, p.Password)
 
 	header := http.Header{}
 	header.Add("Content-Type", "application/json")
 	a.Header = header
 
-	var c http.Client
-	resp, err := c.Do(a)
-	if err != nil {
-		fmt.Printf("Error >>> %s \n", err)
+	if false {
+		var c http.Client
+		resp, err := c.Do(a)
+		if err != nil {
+			fmt.Printf("Error >>> %s \n", err)
+		} else {
+			body, _ := ioutil.ReadAll(resp.Body)
+			fmt.Printf("Resp >>> \n%s\n", body)
+		}
 	} else {
-		body, _ := ioutil.ReadAll(resp.Body)
-		fmt.Printf("Resp >>> \n%s\n", body)
+		fmt.Println(a, p)
 	}
 }
 
 func main() {
 	p := Plivo{
 		Host:     "https://api.plivo.com/v1/Account",
-		User:     "", //user_id
-		Password: "", //user_password
+		User:     "",
+		Password: "",
 	}
 
 	data := map[string]string{
 		"dst":  "",
 		"src":  "",
-		"text": "測試",
+		"text": "",
 	}
 
+	fmt.Println(p)
 	p.send(data)
 }
